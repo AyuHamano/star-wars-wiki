07328c973fb9820fb68de9adc96ecab85f59439d
Ayumi Hamano <98706488+AyuHamano@users.noreply.github.com>
2024-11-04 00:53:23 -0300

    chore: mock de select de filtros

--- src/mock-data/starWarsDirectors.ts
+++ src/mock-data/starWarsDirectors.ts
@@ -1,10 +1,8 @@
-const starWarsDirectors = [
+export const starWarsDirectors = [
+  { value: "", label: "Select a director" },
   { value: "George Lucas", label: "George Lucas" },
   { value: "Irvin Kershner", label: "Irvin Kershner" },
   { value: "Richard Marquand", label: "Richard Marquand" },
-  { value: "J.J. Abrams", label: "J.J. Abrams" },
-  { value: "Rian Johnson", label: "Rian Johnson" },
-  { value: "Ron Howard", label: "Ron Howard" },
 ];
 
 export default starWarsDirectors;
---
+++ src/mock-data/starWarsProducers.ts
@@ -0,0 +1,9 @@
+const starWarsProducers = [
+  { value: "", label: "Select a producer" },
+  { value: "Gary Kurtz", label: "Gary Kurtz" },
+  { value: "Rick McCallum", label: "Rick McCallum" },
+  { value: "Howard G. Kazanjian", label: "Howard G. Kazanjian" },
+  { value: "George Lucas", label: "George Lucas" },
+];
+
+export default starWarsProducers;
