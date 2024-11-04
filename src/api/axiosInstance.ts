29cebdb46b8832dcfb091e17571ad95ef754e54c
Ayumi Hamano <98706488+AyuHamano@users.noreply.github.com>
2024-11-04 00:58:12 -0300

    chore: criação do interceptador axios de exceções

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,8 +1,39 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: 'https://swapi.dev/api/',
-    timeout: 5000
-})
+  baseURL: "https://swapi.dev/api/",
+  timeout: 5000,
+});
 
-export default axiosInstance
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response) {
+      switch (error.response.status) {
+        case 400:
+          alert("Solicitação inválida.");
+          break;
+        case 401:
+          alert("Não autorizado. Por favor, faça login novamente.");
+          break;
+        case 404:
+          alert("Recurso não encontrado.");
+          break;
+        case 500:
+          alert("Erro no servidor. Tente novamente mais tarde.");
+          break;
+        default:
+          alert("Ocorreu um erro inesperado. Tente novamente.");
+          break;
+      }
+    } else if (error.request) {
+      alert("Não foi possível conectar ao servidor. Verifique sua conexão.");
+    } else {
+      alert("Erro ao configurar a requisição. Tente novamente.");
+    }
+
+    return Promise.reject(error);
+  },
+);
+
+export default axiosInstance;
